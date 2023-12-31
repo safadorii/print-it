77d7d998f5a1aa0464c820341c49a61206c7bcb8
safa dorii <safadorai23@gmail.com>
2023-12-31 16:04:37 +0100

    ajout EVENTLISTENER

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -17,34 +18,47 @@ const slides = [
 	}
 ]
 const bannerImg = document.querySelector('.banner-img');
+const tagLine = document.querySelector('#banner p');
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 const dots = document.querySelectorAll('.dot');
 
 let compteur = 0;
 
-function Carousel() {
-    
-    const image = `assets/images/slideshow/${slides[compteur].image}`;
-    bannerImg.src = image;
-    
-
-    const tagLine = slides[compteur].tagLine;
-    document.querySelector('p').innerHTML = tagLine;
-
-   }
-
+// On initialise l'image et la tagline en fonction du tableau
+bannerImg.src = `assets/images/slideshow/${slides[compteur].image}`;
+tagLine.innerHTML = slides[compteur].tagLine;
 
 // le clic sur la flèche gauche
 arrowLeft.addEventListener('click', function () {
-    compteur = (compteur - 1);
-    Carousel(compteur, 'left');
+	// Compteur négatif
+    compteur--;
+	// On retourne à la fin du tableau si le compteur est inférieur à 0
+	if(compteur < 0){
+		compteur = slides.length-1;
+	}
+	// On met à jour l'image et la tagline
+	bannerImg.src = `assets/images/slideshow/${slides[compteur].image}`;
+	tagLine.innerHTML = slides[compteur].tagLine;
+	// Retirer la classe 'dot_selected' de tous les dots
+    dots.forEach(dot => dot.classList.remove('dot_selected'));
+    // Ajouter la classe 'dot_selected' au dot correspondant
+    dots[compteur].classList.add('dot_selected');
 });
 
 // le clic sur la flèche droite
 arrowRight.addEventListener('click', function () {
-    compteur = (compteur + 1) ;
-    Carousel(compteur, 'right');
+	// Compteur positif
+	compteur++;
+	// On remet le compteur à 0 lorsque l'on arrive à la fin du tableau
+	if(compteur == slides.length){
+		compteur = 0;
+	}
+	// On met à jour l'image et la tagline
+	bannerImg.src = `assets/images/slideshow/${slides[compteur].image}`;
+	tagLine.innerHTML = slides[compteur].tagLine;
+	// Retirer la classe 'dot_selected' de tous les dots
+	dots.forEach(dot => dot.classList.remove('dot_selected'));
+    // Ajouter la classe 'dot_selected' au dot correspondant
+    dots[compteur].classList.add('dot_selected');
 });
-
-
